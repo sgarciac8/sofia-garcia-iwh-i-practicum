3924156b02c72e6a4e044e5dd2dc6ef69ed54497
Sofía <sofiagarcia@Sofias-MacBook-Pro.local>
2024-03-27 00:39:26 -0500

    feat: homepage view as a table, homepage route to get custom created object records, install dotenv

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get("/", async (_req, res) => {
+  const objectType = "2-26669455";
+  const cobjects = `https://api.hubspot.com/crm/v3/objects/${objectType}?properties=name&properties=bio&properties=favorite_food`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(cobjects, { headers });
+
+    const data = resp.data.results;
+    res.render("homepage", {
+      title:
+        "Contacts | HubSpot Update Custom Object Form | Integrating With HubSpot I Practicum",
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -41,6 +63,7 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
+
 * * App.post sample
 app.post('/update', async (req, res) => {
     const update = {
@@ -66,6 +89,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
